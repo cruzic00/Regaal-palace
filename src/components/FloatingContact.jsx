68a93f9d099a92e4ba9190e7a5cc32b4af9101fc
cruzic00 <soorajbsz515@gmail.com>
2026-07-30 12:04:07 +0530

    feat: add FloatingContact component with social media and contact links

--- src/components/FloatingContact.jsx
+++ src/components/FloatingContact.jsx
@@ -29,7 +29,7 @@ const items = [
 
 export default function FloatingContact() {
   return (
-    <div className="fixed top-1/4  right-2.5 z-40 sm:left-4">
+    <div className="fixed top-30 right-2.5 z-40 sm:top-28 sm:left-4 sm:right-auto">
       <div className="flex flex-col gap-1.5 sm:gap-2.5">
         {items.map((item) => (
           <a
