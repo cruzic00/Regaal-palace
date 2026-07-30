e62cb00796747341d78d33c07844097296ea5dc1
cruzic00 <soorajbsz515@gmail.com>
2026-07-30 11:29:36 +0530

    feat: add MeetingsEventsPage with event planning features and details

--- src/pages/MeetingsEventsPage.jsx
+++ src/pages/MeetingsEventsPage.jsx
@@ -165,7 +165,7 @@ export default function MeetingsEventsPage() {
       <PageHero
         title="Meetings & Events"
         crumb="Meetings & Events"
-        image="/images/weddings.jpeg"
+        image="images/MeetingEvents.jpeg"
       />
 
       {/* Overview */}
